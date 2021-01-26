e763734e2ea64789859305c4bb152a76023418cc
Tasin Mir <57299816+TasinMir20@users.noreply.github.com>
2021-01-26 23:24:41 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,33 @@
+function showTime() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.querySelector(".LocalTimeDisplay").innerText = time;
+
+    setTimeout(showTime, 1000);
+}
+
+document.querySelector("body").onload = showTime();
